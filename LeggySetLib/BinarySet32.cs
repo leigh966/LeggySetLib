a7579c4dfbd3197da1bdafd8153cc932d7db186c
leigh966 <leigh966@hotmail.com>
2023-11-23 15:47:36 +0000

    first commit

---
+++ LeggySetLib/BinarySet32.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeggySetLib
+{
+    public class BinarySet32 : ISet<int>
+    {
+        private int minNum, maxNum;
+        private uint setBin;
+        public BinarySet32(int minimumNumber, int maximumNumber)
+        {
+            this.minNum = minimumNumber;
+            this.maxNum = maximumNumber;
+
+            int length = maximumNumber - minimumNumber + 1;
+            if (length < 1 )
+            {
+                throw new ArgumentException("Set length cannot be less than 1");
+            }
+            if (length > 32) 
+            {
+                throw new ArgumentException("BinarySet32 cannot handle a range of more than 32 numbers");
+            }
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        private uint GetRepresentingBinary(int number)
+        {
+            // error for < minNum
+            // error for > maxNum
+            int position = number - minNum;
+            return (uint)(1 << position);
+        }
+
+        public bool Add(int item)
+        { 
+            uint rep = GetRepresentingBinary(item);
+            bool contains = ContainsRepresentation(rep);
+            setBin |= rep;
+            return !contains;
+        }
+
+        public void Clear()
+        {
+            setBin = 0;
+        }
+
+        private bool ContainsRepresentation(uint rep)
+        {
+            return (setBin & rep) != 0;
+        }
+
+        public bool Contains(int item)
+        {
+            uint rep = GetRepresentingBinary(item);
+            return ContainsRepresentation(rep);
+        }
+
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExceptWith(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntersectWith(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSubsetOf(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSupersetOf(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Overlaps(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetEquals(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SymmetricExceptWith(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnionWith(IEnumerable<int> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<int>.Add(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestLeggySetLib/TestBinarySet32.cs
@@ -0,0 +1,82 @@
+using System.Diagnostics;
+
+namespace TestLeggySetLib
+{
+    [TestClass]
+    public class TestBinarySet32
+    {
+        [TestMethod]
+        public void TestInitRangeTooBig()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ISet<int> mySet = new BinarySet32(0, 32);
+            });
+            
+        }
+
+        [TestMethod]
+        public void TestInitRangeTooSmall()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ISet<int> mySet = new BinarySet32(1,0);
+            });
+
+        }
+
+        [TestMethod]
+        public void TestInitGoodRange()
+        {
+            ISet<int> mySet = new BinarySet32(1, 32);
+
+        }
+
+        [TestMethod]
+        public void TestNumberInRangeAdded()
+        {
+            ISet<int> mySet = new BinarySet32(1, 32);
+            Assert.IsFalse(mySet.Contains(14));
+            mySet.Add(14);
+            Assert.IsTrue(mySet.Contains(14));
+        }
+
+        private void AddNumbers(ISet<int> ints, int lowest, int highest)
+        {
+            for(int i = lowest; i <= highest; i++)
+            {
+                Assert.IsFalse(ints.Contains(i));
+                ints.Add(i);
+                Assert.IsTrue(ints.Contains(i));
+            }
+        }
+
+
+        [TestMethod]
+        public void TestIsFasterThanHashSet()
+        {
+            Stopwatch hashStopWatch = new Stopwatch();
+            Stopwatch binStopWatch = new Stopwatch();
+
+            // run lots of times to negate the advantage afforded by caching to sets run last
+            for(int i = 0; i < 100; i++)
+            {
+                binStopWatch.Start();
+                ISet<int> binSet = new BinarySet32(1, 32);
+                AddNumbers(binSet, 1, 32);
+                binStopWatch.Stop();
+
+                hashStopWatch.Start();
+                ISet<int> hashSet = new HashSet<int>();
+                AddNumbers(hashSet, 1, 32);
+                hashStopWatch.Stop();
+            }
+
+
+
+            string message = String.Format("HashSet took {0} microseconds. BinarySet32 took {1} microseconds.",
+                hashStopWatch.Elapsed.TotalMicroseconds, binStopWatch.Elapsed.TotalMicroseconds);
+            Assert.IsTrue(hashStopWatch.Elapsed.TotalMicroseconds > binStopWatch.Elapsed.TotalMicroseconds, message);
+        }
+    }
+}
